9f77e34fe505891258a4a3f9b5189046d0087c2a
6v6DS <142214903+6v6DS@users.noreply.github.com>
2024-08-14 11:33:13 +0900

    lv.1 문자열

---
+++ 05/12917/12917.js
@@ -0,0 +1,26 @@
+function solution(s) {
+    var Lower = [];
+    var Upper = [];
+    for(let i = s.length - 1; i > -1; i--){
+        if(s[i] === s[i].toUpperCase()){
+            Upper.push(s[i]);
+        }
+        if(s[i] === s[i].toLowerCase()){
+            Lower.push(s[i]);
+        }
+    }
+    let answer = [];
+    Upper.sort((a, b) => {
+        if(a < b) return 1;
+        if(a > b) return -1;
+        if(a === b) return 0;
+    });
+    Lower.sort((a, b) => {
+        if(a < b) return 1;
+        if(a > b) return -1;
+        if(a === b) return 0;
+    });
+
+    answer.push(Lower, Upper);
+    return answer.join("").replace(/,/g, "");
+}
