3e38e8c9ec333e46c2b3a15eaf4d52d914fa0004
6v6DS <142214903+6v6DS@users.noreply.github.com>
2024-11-06 16:45:22 +0900

    lv.1 완주

---
+++ 09/42576/42576.js
@@ -0,0 +1,13 @@
+function solution(participant, completion) {
+    let answer = [];
+    participant = participant.sort();
+    completion = completion.sort();
+
+    for(let i = 0; i < participant.length; i++){
+        if(participant[i] != completion[i]){
+            answer.push(participant[i])
+            break;
+        }
+    }
+    return answer.join("");
+}
